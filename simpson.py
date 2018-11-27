00659404230d83fbcf3604a44aeab195ead8ab7f
Tommy Myers <tommymyers1101@gmail.com>
2018-11-27 22:52:09 +0000

    Clean up code and allow for custom f(x) function

--- simpson.py
+++ simpson.py
@@ -1,14 +1,13 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 
 import math
 
-def f(x):
-    return math.sin(x) / (math.exp(x) + 4)
-
-def simpson(a, b, n):
+def simpson(f, a, b, n):
     dx = (b - a) / n
-    sum_of_odds = sum([f(a + (dx * x)) for x in range(1, n, 2)])
+    sum_of_odds = sum([f(a + dx * x) for x in range(1, n, 2)])
     sum_of_evens = sum([f(a + dx * x) for x in range(2, n, 2)])
-    return dx / 3 * (f(a) + 4 * sum_of_odds + 2 * sum_of_evens + f(b))
+    return dx / 3 * (f(a) + 4*sum_of_odds + 2*sum_of_evens + f(b))
 
-print(simpson(0, 10, 100))
+f = lambda x: math.sin(x) / (math.exp(x) + 4)
+
+print(simpson(f, 0, 10, 100))
